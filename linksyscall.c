939897657fd809ac48749ea288578040ffd08bec
abhinavpy <py.abhinav2@gmail.com>
2019-01-07 15:52:29 +0530

    Added access, chmod, chown, unlink, link system calls

--- linksyscall.c
+++ linksyscall.c
@@ -4,6 +4,9 @@
 int main()
 {
 	printf("Linking the file reftextforlink.txt, the new file will be linkedfile.txt\n");
-	link("reftextforlink.txt","linkedfile.txt");
+	if(link("reftextforlink.txt","linkedfile.txt") < 0)
+		printf("Error while linking file. link returned -1.");
+	else
+		printf("Linking is successful\n");
 	return 0;
 }
