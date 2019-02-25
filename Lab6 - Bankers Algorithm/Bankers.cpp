4e9f7692deb88cf3aa658cbc396d725d32722b60
abhinavpy <abhinavpy.171co103@nitk.edu.in>
2019-02-25 14:47:39 +0530

    Finished Bankers - Safety Algorithm

--- Lab6 - Bankers Algorithm/Bankers.cpp
+++ Lab6 - Bankers Algorithm/Bankers.cpp
@@ -6,11 +6,11 @@ int main() {
     printf("Enter the number of processes:\n");
     scanf("%d", &nProc);
     printf("Enter the number of resources:\n");
-    scanf("%d" &nRes);
+    scanf("%d", &nRes);
 
     int max[nProc][nRes], allocation[nProc][nRes], need[nProc][nRes];
 
-    printf("Enter the maximum resources needed array for each process as a row:\n");
+    printf("Enter the maximum resources needed array for each process as a row and resources as columns:\n");
     for(int i=0;i<nProc;++i)
         for(int j=0;j<nRes;++j)
             scanf("%d", &max[i][j]);
@@ -18,11 +18,11 @@ int main() {
     printf("Enter the allocated resources in the similar way:\n");
     for(int i=0;i<nProc;++i)
         for(int j=0;j<nRes;++j)
-            scanf("%d", & allocation[i][j]);
+            scanf("%d", &allocation[i][j]);
 
     printf("Need matrix is:\n");
     for(int i=0;i<nProc;++i) {
-        for(int j=0;j<nRes;++i) {
+        for(int j=0;j<nRes;++j) {
             need[i][j] = max[i][j] - allocation[i][j];
             printf("%d ", need[i][j]);
         }
@@ -39,16 +39,39 @@ int main() {
     for(int i=0;i<nRes;++i) {
         sum = 0;
         for(int j=0;j<nProc;++j) {
-            sum += need[i][j];
+            sum += allocation[i][j];
         }
         work[i] = sum;
     }
+
+    printf("The total allocated resources are\n");
+    for(int i=0;i<nRes;++i)
+    	printf("%d ", work[i]);
+    printf("\n");
+
+    for(int i=0;i<nRes;++i)
+    	work[i] = total[i] - work[i];
+
+    printf("The total resources available\n");
+    for(int i=0;i<nRes;++i)
+    	printf("%d ", work[i]);
 
     int finish[nProc];
     for(int i=0;i<nProc;++i)
         finish[i] = 0;
 
-    for(int i=0;i<nProc;++i)
-        for(int j=0;j<nRes;++j)
-            if(finish[i] == 1 ||  
+    printf("The order in which resources are executed are:\n");
+    for(int i=0;i<nProc;++i) {
+        for(int j=0;j<nRes;++j) {
+            if(finish[i] == 1 || need[i][j] > work[j]) {
+            	break;
+            } 
+            else {
+            	finish[i] = 1;
+            	printf("P%d -> ", i);
+            	for(int k=0;k<nRes;++k)
+            		work[k] = allocation[i][k] + work[k];
+            }
+        }
+    }
 }
